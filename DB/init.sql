40045101c8c8396e4904be729a1e7e2d80b1004b
Lexa307 <herodimon@yandex.ru>
2020-02-02 19:06:33 +0300

    [ADD] test db & server auth module
    also created 2/3 new css design of 2 pages

--- DB/init.sql
+++ DB/init.sql
@@ -1,57 +1,72 @@
-create database carddb;
-CREATE TABLE IF NOT EXISTS `carddb`.`Card` (
-  `card_id` INT NOT NULL,
-  `hp_pull` INT NOT NULL,
-  `mana_pull` INT NOT NULL,
-  `damage` INT NOT NULL,
-  `card_name` VARCHAR(45) NOT NULL,
-  `drop_rate` INT NOT NULL,
-  `card_type` VARCHAR(45) NOT NULL,
-  `mana_use` INT NOT NULL,
-  PRIMARY KEY (`card_id`))
-ENGINE = InnoDB;
+CREATE DATABASE IF NOT EXISTS `carddb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `carddb`;
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+
+ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
+ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+-- create database carddb;
+-- CREATE TABLE IF NOT EXISTS `carddb`.`Card` (
+--   `card_id` INT NOT NULL,
+--   `hp_pull` INT NOT NULL,
+--   `mana_pull` INT NOT NULL,
+--   `damage` INT NOT NULL,
+--   `card_name` VARCHAR(45) NOT NULL,
+--   `drop_rate` INT NOT NULL,
+--   `card_type` VARCHAR(45) NOT NULL,
+--   `mana_use` INT NOT NULL,
+--   PRIMARY KEY (`card_id`))
+-- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`pack`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carddb`.`pack` (
-  `pack_id` INT NOT NULL,
-  `card_on_colod` INT(32) NOT NULL,
-  `name_pack` VARCHAR(45) NOT NULL,
-  `Card_card_id` INT NOT NULL,
-  PRIMARY KEY (`pack_id`),
-  INDEX `fk_pack_Card1_idx` (`Card_card_id` ASC),
-  CONSTRAINT `fk_pack_Card1`
-    FOREIGN KEY (`Card_card_id`)
-    REFERENCES `carddb`.`Card` (`card_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- -- -----------------------------------------------------
+-- -- Table `mydb`.`pack`
+-- -- -----------------------------------------------------
+-- CREATE TABLE IF NOT EXISTS `carddb`.`pack` (
+--   `pack_id` INT NOT NULL,
+--   `card_on_colod` INT(32) NOT NULL,
+--   `name_pack` VARCHAR(45) NOT NULL,
+--   `Card_card_id` INT NOT NULL,
+--   PRIMARY KEY (`pack_id`),
+--   INDEX `fk_pack_Card1_idx` (`Card_card_id` ASC),
+--   CONSTRAINT `fk_pack_Card1`
+--     FOREIGN KEY (`Card_card_id`)
+--     REFERENCES `carddb`.`Card` (`card_id`)
+--     ON DELETE NO ACTION
+--     ON UPDATE NO ACTION)
+-- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carddb`.`User` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `nickname` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `gold` INT NOT NULL,
-  `matches` INT NULL,
-  `matches_win` INT NULL,
-  `pack_pack_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`),
-  INDEX `fk_User_pack_idx` (`pack_pack_id` ASC),
-  CONSTRAINT `fk_User_pack`
-    FOREIGN KEY (`pack_pack_id`)
-    REFERENCES `carddb`.`pack` (`pack_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- -- -----------------------------------------------------
+-- -- Table `mydb`.`User`
+-- -- -----------------------------------------------------
+-- CREATE TABLE IF NOT EXISTS `carddb`.`User` (
+--   `user_id` INT NOT NULL AUTO_INCREMENT,
+--   `login` VARCHAR(45) NOT NULL,
+--   `password` VARCHAR(45) NOT NULL,
+--   `nickname` VARCHAR(45) NOT NULL,
+--   `email` VARCHAR(45) NOT NULL,
+--   `gold` INT NOT NULL,
+--   `matches` INT NULL,
+--   `matches_win` INT NULL,
+--   `pack_pack_id` INT NOT NULL,
+--   PRIMARY KEY (`user_id`),
+--   INDEX `fk_User_pack_idx` (`pack_pack_id` ASC),
+--   CONSTRAINT `fk_User_pack`
+--     FOREIGN KEY (`pack_pack_id`)
+--     REFERENCES `carddb`.`pack` (`pack_id`)
+--     ON DELETE NO ACTION
+--     ON UPDATE NO ACTION)
+-- ENGINE = InnoDB;
 
-insert into Card(card_id,hp_pull,mana_pull,damage,card_name,drop_rate,card_type,mana_use) 
-values(1,10,20,5,"test_card",10,"magic_defence",3);
-insert into pack(pack_id,card_on_colod,name_pack,Card_card_id) values(1,10,"test",1);
+-- insert into Card(card_id,hp_pull,mana_pull,damage,card_name,drop_rate,card_type,mana_use) 
+-- values(1,10,20,5,"test_card",10,"magic_defence",3);
+-- insert into pack(pack_id,card_on_colod,name_pack,Card_card_id) values(1,10,"test",1);
--- webServer/index.js
+++ webServer/index.js
@@ -1,99 +1,61 @@
-var app = require('express')();
-var express  = require('express');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-const validate = require('validate.it.js');
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs:60*1000,
   max:10
 });
-let lobby = [];
-let rotationState = {x:0,y:0,z:0}
-// class Lobby {
-//   constructor(player1,player2){
-//     this.player1 = player1;//socket pl1
-//     this.player2 = player2;//socket pl2
-//     this.testcardRotation = {x:0,y:0,z:0}
-//   }
-//   disconnectPlayers(){
-//     this.player1.
-//   }
-// }
-
-//var cookieParser = require('cookie-parser')
-// var mysql = require('mysql');
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   port: "3306",
-//   user: "root",
-//   password: "1234"
-// });
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
-// con.query(`use carddb;`, function (err, result) {
-//   if (err) throw err;
-//   console.log("Result: " + result);
-// });
-app.use(express.static('client'))
-app.use(express.json());
-app.use(limiter);
-//app.use(cookieParser())
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/client/index.html');
-  
-  });
-
-  io.on('connection', function(socket){
-    
-    socket.on('join', function(msg){
-    
-      if(lobby.length<4){
-        
-        lobby.push(msg)
-        let pos = lobby.length-1;
-        console.log(`pos of ${msg} : ${pos}`);
-        io.emit("respond",{msg:"ok", status:"connected",data:rotationState});
-        console.log(lobby);
-        console.log(`${msg} has been connected` );
-        socket.on('disconnect', function(){
-          lobby.splice(pos,1)
-          console.log(lobby);
-          console.log(`${msg} has been disconnected`);
-        });
-        socket.on('cardClick', function(msg){
-          rotationState.y+=1;
-          io.emit("rotate",1);
-        });
-        
-      } else{
-        io.emit("respond",{msg:"not enouth space in lobby", status:"dropped",data:null});
-      }   
-    });
-  
-    
-  });
-
-
-  // app.post('/reg',function (request, response) { 
-  //   if(request.headers["content-length"] > 1e6){
-  //     response.writeHead(416,{'Content-type':'text/plain'});
-  //     response.end();
-  //     request.connection.destroy();
-  //   }else{
-  //     response.writeHead(200,"OK",{'Content-Type':'text/plain'});
-  //     response.end();
-  //   }
-    
-  //    console.log(request.body)
-   
-     
-    
-  // });
-
-  
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '1234',
+	database : 'carddb'
 });
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(express.static('client'))
+app.use(limiter);
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/client/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query(`SELECT * FROM accounts WHERE username = '${username}' AND password ='${password}';` , (err,results)=> {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000);
