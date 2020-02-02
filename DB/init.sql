9980941f0755af85e1a2bad94604841efa548b85
Lexa307 <herodimon@yandex.ru>
2020-02-03 00:26:07 +0300

    completed auth/reg modules

--- DB/init.sql
+++ DB/init.sql
@@ -2,16 +2,17 @@ CREATE DATABASE IF NOT EXISTS `carddb` DEFAULT CHARACTER SET utf8 COLLATE utf8_g
 USE `carddb`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO `accounts` ( `username`, `password`, `email`) VALUES ('test', 'test', 'test@test.com');
 
-ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
-ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+-- ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
+-- ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 
 -- create database carddb;
 -- CREATE TABLE IF NOT EXISTS `carddb`.`Card` (
--- webServer/index.js
+++ webServer/index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+const bcypt = require('bcrypt');
+const saltRounds = 8;
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs:60*1000,
@@ -30,20 +32,30 @@ app.get('/', function(request, response) {
 });
 
 app.post('/auth', function(request, response) {
-	var username = request.body.username;
+	var mail = request.body.mail;
 	var password = request.body.password;
-	if (username && password) {
-		connection.query(`SELECT * FROM accounts WHERE username = '${username}' AND password ='${password}';` , (err,results)=> {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
+	if (mail && password) {
+
+			connection.query(`SELECT password FROM accounts WHERE email = '${mail}';` , (err,results)=> {
+				if (results.length > 0) {
+					console.log(results[0].password);
+					bcypt.compare(password,results[0].password,(err,result)=>{
+						if(result){
+							request.session.loggedin = true;
+							request.session.mail = mail;
+							response.redirect('/home');
+						} else {
+							response.send('Incorrect Username and/or Password!');
+						}	
+					})
+					
+				} else {
+					response.send('Incorrect Username and/or Password!');
+				}			
+			});
+	
+		
 			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
@@ -58,6 +70,37 @@ app.get('/home', function(request, response) {
 		response.send('Please login to view this page!');
 	}
 	response.end();
+});
+
+app.post('/reg', function(request, response) {
+	var nickname = request.body.nickname;
+	var mail = request.body.mail;
+	connection.query(`SELECT * FROM accounts WHERE email = '${mail}';`,(err,results)=>{
+		if (results.length > 0) {
+			response.send('This user alreafy exist');
+			response.end();
+		} else {
+			connection.query(`SELECT * FROM accounts WHERE username = '${nickname}';`,(err,results)=>{
+				if (results.length > 0) {
+					response.send('This nickname already used');
+					response.end();
+				} else {
+					bcypt.genSalt(saltRounds,(err,salt)=>{
+						bcypt.hash(request.body.password,salt,(err,hash)=>{
+							console.log(hash);
+							connection.query(`INSERT INTO accounts (username, password, email) VALUES ('${nickname}', '${hash}', '${mail}');`,(err,results)=>{
+								
+								request.session.loggedin = true;
+								request.session.mail = mail;
+								response.redirect('/home');
+							})
+						})
+					})
+				}
+			
+			}
+		)}
+	})
 });
 
 app.listen(3000);
